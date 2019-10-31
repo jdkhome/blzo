0274ca1370d8b9e89193e6876fcacb709b7726c4
linkji <linkji@linkjideiMac.local>
2019-10-31 11:58:13 +0800

    multi

---
+++ start/blzo-multi/app-core/src/main/java/com/jdkhome/blzo/core/service/UserService.java
@@ -0,0 +1,27 @@
+package com.jdkhome.blzo.core.service;
+
+
+import com.jdkhome.blzo.common.generator.model.User;
+
+/**
+ * create by linkji.
+ * create at 18:01 2019-10-30
+ * [示例]用户业务接口
+ */
+public interface UserService {
+
+    /**
+     * 添加用户
+     *
+     * @param username
+     * @param phone
+     */
+    void add(String username, String phone);
+
+    /**
+     * 查找用户
+     * @param username
+     * @return
+     */
+    User find(String username);
+}
